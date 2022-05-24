8cd2d47d9c7b4bf77ba6ecc326532a32a76ba691
julianabjort <julianabjortiv@gmail.com>
2022-05-24 09:34:33 +0200

    small fix

--- components/ErrorMessage.jsx
+++ components/ErrorMessage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native'
-import { errorMessages } from '../entities/ErrorMessages'
 import colors from '../config/colors';
 
 
